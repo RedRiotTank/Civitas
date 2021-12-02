cfc08d656fe46035854be20fc8c81a1fad61fc81
AlbertoPlaza <albertoplazamontesdm@gmail.com>
2021-12-02 16:03:49 +0100

    mod: JuegoTexto

--- src/JuegoTexto/JuegoTexto.java
+++ src/JuegoTexto/JuegoTexto.java
@@ -4,6 +4,7 @@ import vistaTextualCivitas.VistaTextual;
 import controladorCivitas.Controlador;
 import civitas.CivitasJuego;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -13,12 +14,27 @@ public class JuegoTexto {
 
     public static void main(String[] args) {
         
+        
+        Scanner numJug = new Scanner(System.in);
+        int numjugadores;
+        
+        Scanner nomJug = new Scanner(System.in);
+        String nombreJugador;
+        
         ArrayList<String> nombres = new ArrayList<>();
-        nombres.add("Alberto");
-        nombres.add("Jaime");
-        nombres.add("kitano");
-        nombres.add("Isma");
-              
+        
+        do{
+        System.out.println("Introduzca el número de jugadores (2-4): ");
+        numjugadores = numJug.nextInt();
+        
+        }while(numjugadores > 4 || numjugadores < 2);
+        
+        for(int i=0; i<numjugadores; i++){
+            System.out.print("Introduzca el nombre del jugador número " + (i+1) + ": ");
+            nombreJugador = numJug.next();
+            nombres.add(nombreJugador);
+        }
+                  
         CivitasJuego juego = new CivitasJuego(nombres, false);
         VistaTextual juegoTextual = new VistaTextual(juego);
         Controlador controlador = new Controlador(juego, juegoTextual);
